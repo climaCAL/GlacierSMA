bf62466a3166d0e3aec221637751b9882535fe72
David Giasson <david.giasson@outlook.com>
2024-05-16 02:23:24 -0400

    Added subclasses for shared and exclusive ressources (WIP).

--- GlacierSMA/src/Ressource.h
+++ GlacierSMA/src/Ressource.h
@@ -3,6 +3,7 @@
 class Module;
 
 class Ressource {
+    
     enum State {
         UNAVAILABLE = -1,
         DISABLED = 0,
@@ -11,13 +12,11 @@ class Ressource {
 
     private:
         const char* name;
-        State state;
+        State state = State::DISABLED;
 
     protected:
-        Ressource(const char* name) {
-            this->name = name;
-            this->state = State::DISABLED;
-        }
+        Ressource(const char* name):
+            name(name) {}
 
         bool enable() {
             if (state == State::UNAVAILABLE)
@@ -26,11 +25,9 @@ class Ressource {
             return true;
         }
 
-        bool disable() {
-            if (state == State::UNAVAILABLE)
-                return false;
-            state = State::DISABLED;
-            return true;
+        void disable() {
+            if (state != State::UNAVAILABLE)
+                state = State::DISABLED;
         }
 
         void remove() {
@@ -38,6 +35,57 @@ class Ressource {
         }
 
     public:
-        virtual bool acquire(Module* user) = 0;
-        virtual void release() = 0;
+        State getState() const {
+            return state;
+        }
+
+        virtual bool acquire(const Module* user) = 0;
+        virtual bool release() = 0;
+};
+
+class SharedRessource : public Ressource {
+    private:
+        int nbUsers = 0;
+    
+    protected:
+        SharedRessource(const char* name):
+            Ressource(name) {}
+
+    public:
+        virtual bool acquire(const Module* user) {
+            return enable() ? ++nbUsers : false;
+        }
+
+        virtual bool release() { // Warning: This method does not check if the same user releases more than once!
+            if (nbUsers == 0)
+                return false;
+            else if (--nbUsers == 0)
+                disable();
+            return true;
+        }
+};
+
+class ExclusiveRessource : public Ressource {
+    private:
+        const Module* usedBy = nullptr;
+    
+    protected:
+        ExclusiveRessource(const char* name):
+            Ressource(name) {}
+        
+    public:
+        virtual bool acquire(const Module* user) { // NB: This will return false if you try to reacquire the same ressource.
+            if (usedBy != nullptr || !enable())
+                return false;
+            usedBy = user;
+            return true;
+        }
+
+        virtual bool release() { // Warning: This method does not check if the owner is the one releasing the ressource!
+            if (usedBy == nullptr)
+                return false;
+            disable();
+            usedBy = nullptr;
+            return true;
+        }
 };
